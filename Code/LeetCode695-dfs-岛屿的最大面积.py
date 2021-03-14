7b36bddf5573714230f3d6428d4e5ed486b5cb2a
huasong.shan <huasong.shan@jd.com>
2021-03-14 08:39:50 -0700

    .

---
+++ Code/LeetCode695-dfs-岛屿的最大面积.py
@@ -0,0 +1,59 @@
+from pprint import pprint
+
+class Solution(object):
+
+    def maxAreaOfIsland2(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+        row, col = len(grid), len(grid[0])
+        def dfs(i, j):
+            if 0 <= i <= row - 1 and 0 <= j <= col - 1 and grid[i][j]:
+                grid[i][j] = 0 # 重要，防止再次搜索
+                return  1 + dfs(i - 1, j) + dfs(i + 1, j) + dfs(i, j - 1) + dfs(i, j + 1)
+            return 0
+        return max(dfs(i, j) for i in range(row) for j in range(col))
+
+    def maxAreaOfIsland(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+        self.res = 0
+        self.island = 0
+        M, N = len(grid), len(grid[0])
+        for i in range(M):
+            for j in range(N):
+                if grid[i][j]:
+                    self.dfs(grid, i, j)
+                    self.res = max(self.res, self.island)
+                    self.island = 0
+        return self.res
+    
+    def dfs(self, grid, i, j): # ensure grid[i][j] == 1
+        M, N = len(grid), len(grid[0])
+        grid[i][j] = 0
+        self.island += 1
+        dirs = [(0, 1), (0, -1), (-1, 0), (1, 0)]
+        for d in dirs:
+            x, y = i + d[0], j + d[1]
+            if 0 <= x < M and 0 <= y < N and grid[x][y]:
+                self.dfs(grid, x, y)
+s = Solution()
+
+t1 =[[0,0,1,0,0,0,0,1,0,0,0,0,0],
+ [0,0,0,0,0,0,0,1,1,1,0,0,0],
+ [0,1,1,0,1,0,0,0,0,0,0,0,0],
+ [0,1,0,0,1,1,0,0,1,0,1,0,0],
+ [0,1,0,0,1,1,0,0,1,1,1,0,0],
+ [0,0,0,0,0,0,0,0,0,0,1,0,0],
+ [0,0,0,0,0,0,0,1,1,1,0,0,0],
+ [0,0,0,0,0,0,0,1,1,0,0,0,0]]
+
+t2 = [[0,0,0,0,0,0,0,0]]
+
+pprint(t1)
+print(s.maxAreaOfIsland2(t1))
+#print(s.maxAreaOfIsland(t1))
+print(t2)
