8754eb79332189409b64429f3de5475890870fe7
huasong.shan <huasong.shan@jd.com>
2021-03-15 22:16:02 -0700

    .

--- Code/Stack-LeetCode394-DecodeString.py
+++ Code/Stack-LeetCode394-DecodeString.py
@@ -3,6 +3,7 @@ s = "3[a2[c]]"#, return "accaccacc".
 s = "2[abc]3[cd]ef"#, return "abcabccdcdcdef".
 def decodeString(s):
     #遇到’[‘就把之前的字符串进行进栈操作。遇到’]'进行出栈操作。
+    #curstring = prestring + prenum * curstring，prestring是前面的字符串，prenum * curstring是这一步骤结束之后的字符串，所以是前面的字符串+现在的字符串得到目前已有的字符串。
         curnum = 0
         curstring = ''
         stack = []
