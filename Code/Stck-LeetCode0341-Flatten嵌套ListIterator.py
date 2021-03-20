a9b2d9ed21d045189af0b52d1b91adfa4f60cd3d
huasong.shan <huasong.shan@jd.com>
2021-03-19 22:07:48 -0700

    .

---
+++ Code/Stck-LeetCode0341-Flatten嵌套ListIterator.py
@@ -0,0 +1,52 @@
+# """
+# This is the interface that allows for creating nested lists.
+# You should not implement it, or speculate about its implementation
+# """
+#class NestedInteger(object):
+#    def isInteger(self):
+#        """
+#        @return True if this NestedInteger holds a single integer, rather than a nested list.
+#        :rtype bool
+#        """
+#
+#    def getInteger(self):
+#        """
+#        @return the single integer that this NestedInteger holds, if it holds a single integer
+#        Return None if this NestedInteger holds a nested list
+#        :rtype int
+#        """
+#
+#    def getList(self):
+#        """
+#        @return the nested list that this NestedInteger holds, if it holds a nested list
+#        Return None if this NestedInteger holds a single integer
+#        :rtype List[NestedInteger]
+#        """
+#Input: [[1,1],2,[1,1]]
+#Output: [1,1,2,1,1]
+class NestedIterator(object):
+    def __init__(self, nestedList):
+        """
+        Initialize your data structure here.
+        :type nestedList: List[NestedInteger]
+        """
+        self.stack = []
+        while nestedList:
+            self.stack.append(nestedList.pop())
+
+    def next(self):
+        """
+        :rtype: int
+        """
+        return self.stack.pop().getInteger()     
+
+    def hasNext(self):
+        """
+        :rtype: bool
+        """
+        while self.stack:
+            top = self.stack[-1]
+            if top.isInteger():
+                return True
+            self.stack = self.stack[:-1] + top.getList()[::-1]
+        return False
