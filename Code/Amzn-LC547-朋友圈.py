d569ef9ca5f687ea679591ab45fc8f6e0510df6b
huasong.shan <huasong.shan@jd.com>
2021-03-22 11:44:53 -0700

    .

--- Code/Amzn-LC547-朋友圈.py
+++ Code/Amzn-LC547-朋友圈.py
@@ -1,32 +1,26 @@
 from pprint import pprint
+def findCircleNum(M):
+        def dfs(M, curr, n):
+            print('in', curr)
+            for i in range(n):
+                if M[curr][i] == 1:
+                    M[curr][i] = M[i][curr] = 0
+                    dfs(M, i, n)
+            print('out', curr)
 
+        n = len(M)
+        ans = 0
+        for i in range(n):
+            if M[i][i] == 1:
+                ans += 1
+                dfs(M, i, n)
+
+        return ans
 t1 = [[1,1,0], [1,1,0], [0,0,1]]
+t1 = [[1,0,1], [0,1,0], [1,0,1]]
 t2 = [[1,1,0], [1,1,1], [0,1,1]]
 for r in t1:
     print(r)
-
-
-def findCircleNum(M):
-        """
-        :type M: List[List[int]]
-        :rtype: int
-        """
-        cnt, N = 0, len(M)
-        vset = set()
-        def dfs(n):
-            for x in range(N):
-                if M[n][x] and x not in vset:
-                    vset.add(x)
-                    dfs(x)
-
-
-        for x in range(N):
-            if x not in vset:
-                cnt += 1
-                dfs(x)
-        return cnt
 print(findCircleNum(t1))
-
-
-for r in t2:
-    print(r)
+#for r in t2:
+#    print(r)
